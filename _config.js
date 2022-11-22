79988b6f17682524b784451a9d92e6fec4695f5b
rodneykabiru <rodneymararo@yahoo.com>
2022-11-22 19:01:58 +0300

    Updated _config.js

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://rodney:rodney123@gallery.str4ax1.mongodb.net/darkroom?retryWrites=true',
-    development: 'mongodb+srv://rodney:rodney123@gallery.str4ax1.mongodb.net/darkroom-dev?retryWrites=true',
-    test: 'mongodb+srv://rodney:rodney123@gallery.str4ax1.mongodb.net/darkroom-test?retryWrites=true',
+    production: 'mongodb+srv://rodney:rodney123@gallery.8wwunab.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://rodney:rodney123@gallery.8wwunab.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://rodney:rodney123@gallery.8wwunab.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
