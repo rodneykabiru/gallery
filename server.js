332ffd7f985e3df820b82b12ff047e8207e0e044
rodneykabiru <rodneymararo@yahoo.com>
2022-11-22 20:14:21 +0300

    Updated server.js

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use('/image', image);
 
 
  
-const PORT = process.env.PORT || 5100;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT,() =>{
     console.log(`Server is listening at http://localhost:${PORT}`)
 });
