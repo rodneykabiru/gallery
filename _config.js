ad380bb58475b28f29cd33ee6a13dd08f483081e
rodneykabiru <rodneymararo@yahoo.com>
2022-11-22 20:05:18 +0300

    Updated _config.js

--- _config.js
+++ _config.js
@@ -4,6 +4,6 @@ var config = {}
 config.mongoURI = {
     production: 'mongodb+srv://rodney:rodney123@gallery.8wwunab.mongodb.net/darkroom?retryWrites=true&w=majority',
     development: 'mongodb+srv://rodney:rodney123@gallery.8wwunab.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://rodney:rodney123@gallery.8wwunab.mongodb.net/darkromm-test?retryWrites=true&w=majority',
+    test: 'mongodb+srv://rodney:rodney123@gallery.8wwunab.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
